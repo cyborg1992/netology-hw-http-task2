2173e1254f6a4dedc5d5a0e95970f83cadb9d791
Alexander Khlyntcev <khlyntcevsasha@gmail.com>
2022-07-18 09:07:40 +0300

    rename field

--- src/main/java/ru/netology/PictureOfDay.java
+++ src/main/java/ru/netology/PictureOfDay.java
@@ -7,8 +7,8 @@ public class PictureOfDay {
     private final String date;
     private final String explanation;
     private final String hdurl;
-    private final String media_type;
-    private final String service_version;
+    private final String mediaType;
+    private final String serviceVersion;
     private final String title;
     private final String url;
 
@@ -17,8 +17,8 @@ public class PictureOfDay {
             @JsonProperty("date") String date,
             @JsonProperty("explanation") String explanation,
             @JsonProperty("hdurl") String hdurl,
-            @JsonProperty("media_type") String media_type,
-            @JsonProperty("service_version") String service_version,
+            @JsonProperty("media_type") String mediaType,
+            @JsonProperty("service_version") String serviceVersion,
             @JsonProperty("title") String title,
             @JsonProperty("url") String url
     ) {
@@ -26,8 +26,8 @@ public class PictureOfDay {
         this.date = date;
         this.explanation = explanation;
         this.hdurl = hdurl;
-        this.media_type = media_type;
-        this.service_version = service_version;
+        this.mediaType = mediaType;
+        this.serviceVersion = serviceVersion;
         this.title = title;
         this.url = url;
     }
@@ -42,8 +42,8 @@ public class PictureOfDay {
                 "date='" + date + "'\n" +
                 "explanation='" + explanation + "'\n" +
                 "hdurl='" + hdurl + "'\n" +
-                "media_type='" + media_type + "'\n" +
-                "service_version='" + service_version + "'\n" +
+                "media_type='" + mediaType + "'\n" +
+                "service_version='" + serviceVersion + "'\n" +
                 "title='" + title + "'\n" +
                 "url='" + url + '\'';
     }
